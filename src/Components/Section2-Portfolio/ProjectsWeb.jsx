86596d6e7c16eb35bd7eeb772befd1613f987a93
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2026-02-07 22:04:55 +0530

    Change image URL for Animated Portfolio project
    Updated the image URL for the Animated Portfolio project.

--- src/Components/Section2-Portfolio/ProjectsWeb.jsx
+++ src/Components/Section2-Portfolio/ProjectsWeb.jsx
@@ -4,7 +4,7 @@ const Projects=()=>{
 
     const Details=[
         {projectname:"Airbnb (Travel Application)",projectdomain:"Web Development",navigationlink:"https://airbnb-miniversion.onrender.com/listing",imgurl:"/Airbnb.avif"},
-        {projectname:"Animated Portfolio",projectdomain:"Web Development",navigationlink:"https://react-portfolio-animated.onrender.com/",imgurl:"/Blog-Post-Portfolio.jpg"},
+        {projectname:"Animated Portfolio",projectdomain:"Web Development",navigationlink:"https://react-portfolio-animated.onrender.com/",imgurl:"https://mir-s3-cdn-cf.behance.net/projects/404/28ceb2220930093.Y3JvcCwxMDA3LDc4OCwxOTcsMA.jpg"},
         {projectname:"Text Editor Package (NPM)",projectdomain:"Web Development",navigationlink:"https://www.npmjs.com/package/flowtext",imgurl:"/rich-text-editor-for-react-min.png"},
         {projectname:"Stock Scope (Market Data)",projectdomain:"Web Development",navigationlink:"/a",imgurl:"/stock-market.webp"},
     ];
