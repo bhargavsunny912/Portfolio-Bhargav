a224f52f1c2e62fd4c17001ed53f5e4e05796080
Bhargav Penta <BhargavPenta@gmail.com>
2026-02-28 21:47:43 +0530

    Adding urls

--- src/Components/Section2-Portfolio/ProjectsAll.jsx
+++ src/Components/Section2-Portfolio/ProjectsAll.jsx
@@ -3,12 +3,12 @@ import Project from "./Project";
 const Projects=()=>{
 
     const Details=[
+        {projectname:"Stock Scope (Market Data)",projectdomain:"Web Development",navigationlink:"https://stock-scope-frontend-nine.vercel.app/",imgurl:"/stock-market.webp"},
         {projectname:"Airbnb (Travel Application)",projectdomain:"Web Development",navigationlink:"https://airbnb-miniversion.onrender.com/listing",imgurl:"/Airbnb.avif"},
         {projectname:"Animated Portfolio",projectdomain:"Web Development",navigationlink:"https://react-portfolio-animated.onrender.com/",imgurl:"https://mir-s3-cdn-cf.behance.net/projects/404/28ceb2220930093.Y3JvcCwxMDA3LDc4OCwxOTcsMA.jpg"},
         {projectname:"Safe Video Transmission",projectdomain:"Application",navigationlink:"/a",imgurl:"/Video-Encryption.webp"},
         {projectname:"Text Editor Package (NPM)",projectdomain:"Web Development",navigationlink:"https://www.npmjs.com/package/flowtext",imgurl:"/rich-text-editor-for-react-min.png"},
-        {projectname:"Stock Scope (Market Data)",projectdomain:"Web Development",navigationlink:"/a",imgurl:"/stock-market.webp"},
-        {projectname:"Text To Image Converter",projectdomain:"Web Development",navigationlink:"/a",imgurl:"/Text-to-image.webp"},
+        {projectname:"Text To Image Converter",projectdomain:"Application",navigationlink:"/a",imgurl:"/Text-to-image.webp"},
     ];
 
     return (
--- src/Components/Section2-Portfolio/ProjectsWeb.jsx
+++ src/Components/Section2-Portfolio/ProjectsWeb.jsx
@@ -3,10 +3,10 @@ import Project from "./Project";
 const Projects=()=>{
 
     const Details=[
+        {projectname:"Stock Scope (Market Data)",projectdomain:"Web Development",navigationlink:"https://stock-scope-frontend-nine.vercel.app/",imgurl:"/stock-market.webp"},
         {projectname:"Airbnb (Travel Application)",projectdomain:"Web Development",navigationlink:"https://airbnb-miniversion.onrender.com/listing",imgurl:"/Airbnb.avif"},
         {projectname:"Animated Portfolio",projectdomain:"Web Development",navigationlink:"https://react-portfolio-animated.onrender.com/",imgurl:"https://mir-s3-cdn-cf.behance.net/projects/404/28ceb2220930093.Y3JvcCwxMDA3LDc4OCwxOTcsMA.jpg"},
         {projectname:"Text Editor Package (NPM)",projectdomain:"Web Development",navigationlink:"https://www.npmjs.com/package/flowtext",imgurl:"/rich-text-editor-for-react-min.png"},
-        {projectname:"Stock Scope (Market Data)",projectdomain:"Web Development",navigationlink:"/a",imgurl:"/stock-market.webp"},
     ];
 
     return (
